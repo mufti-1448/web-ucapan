606eeedb31a50c3ea699f8fdd84878585f4abfa9
mufti-1448 <mufti0480@gmail.com>
2026-02-16 15:29:12 +0700

    Add fitur Foto

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const btnWa = document.getElementById("btnWa");
 const namaPengirim = document.getElementById("namaPengirim");
 namaPengirim.textContent = nama;
 
+const btnKamera = document.getElementById("btnKamera");
+const kameraBox = document.getElementById("kameraBox");
+const video = document.getElementById("video");
+const btnFoto = document.getElementById("btnFoto");
+const canvas = document.getElementById("canvas");
+const hasilFoto = document.getElementById("hasilFoto");
+
+let stream;
+
+
 tombol.addEventListener("click", function () {
   jumlahKlik++;
 
@@ -30,6 +40,8 @@ tombol.addEventListener("click", function () {
     tombol.style.display = "none";
     teksCounter.style.display = "none";
     btnWa.style.display = "inline-block";
+    btnKamera.style.display = "inline-block";
+
 
     for (let i = 0; i < 20; i++) {
       const confetti = document.createElement("div");
@@ -44,8 +56,36 @@ tombol.addEventListener("click", function () {
 });
 
 btnWa.addEventListener("click", function () {
-  const pesan = `Terima kasih ya ${nama} 💖 Website ucapannya keren banget!`;
+  const pesan = `Terima kasih ya ${nama} 💖 Aku sudah lihat ucapannya dan ini fotoku 😄`;
   const url = "https://wa.me/?text=" + encodeURIComponent(pesan);
   window.location.href = url;
+});
+
+
+btnKamera.addEventListener("click", async function () {
+  kameraBox.style.display = "block";
+
+  stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "user" },
+    audio: false,
+  });
+
+  video.srcObject = stream;
+});
+
+btnFoto.addEventListener("click", function () {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0);
+
+  const foto = canvas.toDataURL("image/png");
+  hasilFoto.src = foto;
+
+  hasilFoto.style.display = "block";
+  kameraBox.style.display = "none";
+
+  stream.getTracks().forEach((track) => track.stop());
 });
 
